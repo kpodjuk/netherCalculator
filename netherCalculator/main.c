c88b91bf6cf6c268f9fdd087e9cde34fff880486
kpodjuk <62662345+kpodjuk@users.noreply.github.com>
2020-07-23 14:12:20 +0200

    Dodaj pliki projektów.

---
+++ netherCalculator/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main() {
+
+	do {
+		double x, y, z, rx, ry, rz;
+		char choice;
+		printf("What do you want to input?\n");
+		printf("o - overworld coordinates\n");
+		printf("n - nether coordinates\n");
+		printf("q - quit\n");
+		scanf_s("%s", &choice, 1);
+
+		if ('o' == choice) {
+
+			printf("Input x overworld cooordinate: \n");
+			scanf_s("%lf", &x);
+			printf("Input y overworld coorodinate: \n");
+			scanf_s("%lf", &y);
+			printf("Input z overworld coordinate: \n");
+			scanf_s("%lf", &z);
+
+			printf("Results in nether: \n");
+
+			rx = floor(x);
+			rx = (rx / 8);
+			ry = floor(y);
+			rz = floor(z);
+			rz = (z / 8);
+			printf("X:%0.1lf\n", rx);
+			printf("Y:%0.1lf\n", ry);
+			printf("Z:%0.1lf\n", rz);
+		}
+		if (choice == 'n') {
+			printf("Input x nether coordinate: \n");
+			scanf_s("%lf", &x);
+			printf("Input y nether coordinate: \n");
+			scanf_s("%lf", &y);
+			printf("Input z nether coordinate: \n");
+			scanf_s("%lf", &z);
+
+			printf("Results in overworld: \n");
+
+			rx = floor(x);
+			rx = (rx * 8);
+			ry = floor(y);
+			rz = floor(z);
+			rz = (z * 8);
+			printf("X:%0.1lf\n", rx);
+			printf("Y:%0.1lf\n", ry);
+			printf("Z:%0.1lf\n", rz);
+		}
+		if (choice == 'q') {
+			break;
+		}
+
+
+	} while (1);
+	return 0;
+}
