f1d0793e645b5285bd825458ac4356eb15a80991
kpodjuk <62662345+kpodjuk@users.noreply.github.com>
2020-07-26 13:47:46 +0200

    commit

--- netherCalculator/main.c
+++ netherCalculator/main.c
@@ -1,19 +1,54 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <ctype.h>
 
+void CleanBuffer() {
+	int n;
+	while ((n = getchar()) != EOF && n != '\n');
+}
 
 char GetStuff(void) {
 	char c;
 	scanf_s("%c", &c, 1);
 	getchar();
 	while ((c != 'o') && (c != 'n') && (c != 'q')) {
-		printf("invalid input, enter again: ");
+		printf("Invalid input");
 		scanf_s("%c", &c, 1);
 		getchar();
 	}
 	return c;
 }
+
+
+void calculateOverworld(double x, double y, double z) {
+	double rx, ry, rz;
+	printf("Results in nether: \n");
+	rx = floor(x);
+	rx = (rx / 8);
+	ry = floor(y);
+	rz = floor(z);
+	rz = (z / 8);
+	printf("X:%0.1lf\n", rx);
+	printf("Y:%0.1lf\n", ry);
+	printf("Z:%0.1lf\n", rz);
+}
+
+void calculateNether(double x, double y, double z) {
+	double rx, ry, rz;
+	printf("Results in overworld: \n");
+	rx = floor(x);
+	rx = (rx * 8);
+	ry = floor(y);
+	rz = floor(z);
+	rz = (z * 8);
+	printf("X:%0.1lf\n", rx);
+	printf("Y:%0.1lf\n", ry);
+	printf("Z:%0.1lf\n", rz);
+}
+
+
+
 
 
 
@@ -22,33 +57,21 @@ int main() {
 	printf("o - overworld coordinates\n");
 	printf("n - nether coordinates\n");
 	printf("q - quit\n");
-	while(1) {
-		double x, y, z, rx, ry, rz;
+		double x, y, z;
 		char choice = GetStuff();
-
-
-
+		printf("You have entered: %c\n", GetStuff());
 
 		if ('o' == choice) {
-
 			printf("Input x overworld coordinates: \n");
 			scanf_s("%lf", &x);
 			printf("Input y overworld coordinates: \n");
 			scanf_s("%lf", &y);
 			printf("Input z overworld coordinates: \n");
 			scanf_s("%lf", &z);
-
-			printf("Results in nether: \n");
-
-			rx = floor(x);
-			rx = (rx / 8);
-			ry = floor(y);
-			rz = floor(z);
-			rz = (z / 8);
-			printf("X:%0.1lf\n", rx);
-			printf("Y:%0.1lf\n", ry);
-			printf("Z:%0.1lf\n", rz);
+			calculateOverworld(x, y, z);
 		}
+
+
 		else if (choice == 'n') {
 			printf("Input x nether coordinate: \n");
 			scanf_s("%lf", &x);
@@ -56,26 +79,10 @@ int main() {
 			scanf_s("%lf", &y);
 			printf("Input z nether coordinate: \n");
 			scanf_s("%lf", &z);
-
-			printf("Results in overworld: \n");
-
-			rx = floor(x);
-			rx = (rx * 8);
-			ry = floor(y);
-			rz = floor(z);
-			rz = (z * 8);
-			printf("X:%0.1lf\n", rx);
-			printf("Y:%0.1lf\n", ry);
-			printf("Z:%0.1lf\n", rz);
+			calculateNether(x, y, z);
 		}
-		else if (choice == 'q') {
-			break;
-		}
-		else {
-			printf("Wrong input");
-		}
+		else if (choice == 'Q') {
 
-
-	}
+		}
 	return 0;
 }
